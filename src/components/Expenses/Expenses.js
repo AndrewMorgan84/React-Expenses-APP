9bd6a0af785c3cdda305fbdac70b3d7598f34393
Andrew <andrew.morgan@e.org>
2022-01-29 17:44:44 +0000

    Added monthly expense chart

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import Card from '../UI/Card.js';
 import ExpensesFilter from './ExpensesFilter';
 import { useState } from 'react/cjs/react.development';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2021');
@@ -18,6 +19,7 @@ const Expenses = (props) => {
     <div>
       <Card className='expenses'>
           <ExpensesFilter selected={filteredYear} onChangeYearDropdown={filterChangeHandler}/>
+          <ExpensesChart expenses={filteredExpenses}/>
           <ExpensesList items={filteredExpenses}/>
       </Card>
     </div>  
